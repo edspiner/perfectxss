d1e849c008339c2124dcbbfb63cc43fecd77e8ba
Edi Skraba <edspiner@gmail.com>
2018-08-28 14:25:56 +0200

    Update payload.js

--- payload.js
+++ payload.js
@@ -8,5 +8,9 @@
 
 // Get cookies
 // Get HTTP headers from current document.location
+var xhr = new XMLHttpRequest();
+var headers = XMLHttpRequest.getAllResponseHeaders();
+
 // if user logged (search for keywords like "logout", "user", "profile" etc, check for common session cookies, send request without credentials)
 // a return of request without credentials should return a redirect to login so we can get the Login URL from this response
+
