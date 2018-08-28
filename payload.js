02f6b22cbe4e3c7cd9209bd9e2a46a9806491f12
Edi Skraba <edspiner@gmail.com>
2018-08-28 14:25:07 +0200

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,12 @@
+// Include required libs like html2canvas for screenshot
+
+// bypass CSP
+
+// Screenshot
+
+// Get DOM
+
+// Get cookies
+// Get HTTP headers from current document.location
+// if user logged (search for keywords like "logout", "user", "profile" etc, check for common session cookies, send request without credentials)
+// a return of request without credentials should return a redirect to login so we can get the Login URL from this response
